4e12f0b016bcaa603145afb2074cecad4bde67f1
ameni-selmi <ameniselmi456@gmail.com>
2025-02-05 21:49:46 +0100

    update logo position

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Logo = () => {
   return (
-    <div className="flex items-center justify-center sm:justify-end mt-2 absolute top-0 left-1/2 transform -translate-x-1/2 sm:left-auto sm:left-1/4">
+    <div className="flex items-center justify-center sm:justify-end mt-2 absolute top-0 left-1/2 transform -translate-x-1/2 sm:left-1/4">
         <Image
             src="/favicon.ico"
             alt="Sshorty Logo"
